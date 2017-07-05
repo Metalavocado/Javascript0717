10d23b4cf0b30da6f5e99aea46a7f377ee30ca2c
Metalavocado <metalavocado.games@gmail.com>
2017-07-05 14:54:12 +1000

    Added the initial value setting lines (Setting the name and role, as well as their formatting)

--- ResumeWebsite/js/resumeBuilder.js
+++ ResumeWebsite/js/resumeBuilder.js
@@ -1,3 +1,17 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//Value declarations
+var name = "Rohan Cruickshank";
+var role = "Unity Developer";
+
+
+
+
+
+
+
+//Value formatting
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+//Display values on website
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
